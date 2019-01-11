046f3193ceff6402fddec4e19869867c9aa11646
Marko <marko.miloradovic@barsanti.edu.it>
2019-01-11 09:16:17 +0100

    11.10.19 - LED rosso

--- angelaGame/angelaGame.ino
+++ angelaGame/angelaGame.ino
@@ -1,9 +1,15 @@
 void setup() {
-  // put your setup code here, to run once:
+// put your setup code here, to run once:
+
+int ledRosso = 8;
+int ledBlu = 9;
 
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
-
+    if(digitalRead(ledRosso) == HIGH)
+    {
+        digitalWrite(ledRosso) = LOW;  
+    }
 }
