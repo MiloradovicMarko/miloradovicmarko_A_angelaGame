e374e0fecbb166e629cf1e92c4dab0179ba1b2c0
Marko <marko.miloradovic@barsanti.edu.it>
2019-01-18 09:14:16 +0100

    18.01.19 Modifica per Angela
    Inserimento di un metodo per i valori messi in input,definizione di turni

--- angelaGame/angelaGame.ino
+++ angelaGame/angelaGame.ino
@@ -1,15 +1,58 @@
+#include <LiquidCrystal.h>
+int turnoGioco = 0;
+int numeroSceltoPre = 0;
+int punteggioScelto = 0;
 void setup() {
 // put your setup code here, to run once:
+Serial.begin(9600);
 
-int ledRosso = 8;
-int ledBlu = 9;
 
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
-    if(digitalRead(ledRosso) == HIGH)
+  if(turnoGioco == 0)
+  {
+    int contenitore[] = {0,1,2,3,4,5,6};
+    numeroSceltoPre = contenitore[Serial.parseInt()]; // definire Posizione basata su input utente 
+    Serial.println("Scegli un numero");
+    if(numeroSceltoPre == 0)
     {
-        digitalWrite(ledRosso) = LOW;  
+      turnoGioco == -1;
     }
+    else
+    {
+      punteggioScelto -= numeroSceltoPre;
+    }
+  }
+  if(turnoGioco == -1)
+  {
+      Selezione(Serial.parseInt()); // definire Posizione da Utente       
+  }
+  if(turnoGioco == 1)
+  {
+      Selezione(Serial.parseInt()); // definire Posizione da Utente
+  }
+    
+  }
+
+void Selezione(int posizione)
+{
+if(numeroSceltoPre == 1 || numeroSceltoPre == 6)
+      {
+        int contenitore[] ={2,3,4,5};
+        numeroSceltoPre = contenitore[posizione-2];
+      }
+      if(numeroSceltoPre == 2 || numeroSceltoPre == 5)
+      {
+        int contenitore[] ={1,3,4,6};
+        numeroSceltoPre = contenitore[posizione];
+      }
+      if(numeroSceltoPre == 3 || numeroSceltoPre == 4)
+      {
+        int contenitore[] ={1,2,5,6};
+        numeroSceltoPre = contenitore[posizione]; 
+      }
 }
+
+
